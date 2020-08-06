723403102815c7193ecf6561a075a40fe4fe8878
yosafatjuantp <3103118142@student.smktelkom-pwt.sch.id>
2020-08-06 14:57:56 +0700

    tugas

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+
+class AuthController extends Controller {
+    
+    public function me() {
+        return [
+            "nis" => 3103118142,
+            "name" => "Yosafat Juan Timotius Prasetya",
+            "gender" => "Laki-laki",
+            "phone" => 081316516200,
+            "class" => "XII RPL 4"
+        ];
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,12 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+//Route::get('/', function () {
+  //  return view('welcome');
+// });
+
+Route::get('/',function(){
+    return"XII RPL 4";
 });
+
+Route::get('/me','AuthController@me');
